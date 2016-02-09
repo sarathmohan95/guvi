4c351a6ab551259e1b1057b1da99a7f4a8c3aff9
sarathmohan95 <sarathkumar.mohan6@gmail.com>
2016-02-09 19:43:20 +0530

    Create toupper.cs

---
+++ toupper.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "sarath";
+            string upper = text.ToUpper();
+            Console.WriteLine("",upper);
+     
+} 
+
+        }
+    }
