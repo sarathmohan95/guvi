8b0df39f717c35dccf8d567d33c9f17f8f951422
sarathmohan95 <sarathkumar.mohan6@gmail.com>
2016-02-09 19:20:53 +0530

    Create strtoint.cs

---
+++ strtoint.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "500";
+	int num = int.Parse(text);
+	Console.WriteLine(num);
+
+            
+           
+} 
+
+        }
+    }
